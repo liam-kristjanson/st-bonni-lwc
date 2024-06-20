121ad232aedc352a9fcf3ae69876e43ecf518c11
bopbaby <ayoibk25@gmail.com>
2024-06-20 15:01:25 -0500

    Made changes to promotion offerings

--- frontend/src/routes/Serves.tsx
+++ frontend/src/routes/Serves.tsx
@@ -1,158 +1,39 @@
 //import React from "react";
 //import Button from 'react-bootstrap/Button';
 
+import PromotionOffered from "../components/PromotionOffered";
 
-const PromotionOffered = () => {
+
+const Serves = () => {
     return(
         <>
-        <div className="container gap-6 py-5 mx-auto md:container-col-3 max-w-l">
-          <span className="position-relative d-flex align-items-stretch w-100 bg-light-subtle rounded square border border-4 border-success-subtle shadow-lg">
-                    <div className="d-flex flex-column gap-4 px-5 pt-16 pb-8 mx-auto rounded px-md-4 overflow-hidden ">
-                        <div className=" position-absolute top-0 right-0">
+          <div className=" container">
+            <div className="row">
 
-                        </div>
+                <div className="col-xl-4 col-md-6 col-sm-12">
+                <PromotionOffered price="$400" mainHeading={"Basic Offer"} promotionContent={"All things offered"} promotionSubscription={"Brief Description"}/>
+                </div>
+                
+         
+                
+                <div className="col-xl-4 col-md-6 col-sm-12">
+                <PromotionOffered price="$400" mainHeading={"Basic Offer"} promotionContent={"All things offered"} promotionSubscription={"Brief Description"}/>
+                </div>
 
-                     
-                    </div>
-            </span> 
-
+                <div className="col-xl-4 col-md-6 col-sm-12">
+                <PromotionOffered price="$400" mainHeading={"Basic Offer"} promotionContent={"All things offered"} promotionSubscription={"Brief Description"}/>
+                </div>
            
-            
-
-            <div className="d-flex justify-content-center align-content-center text-bg-success">
-                 lorem ipsum
-            </div>
-            
-           
+         </div>
 
         </div>
-
-
-    <div className="row justify-content-center py-4">  
-
-        <div className="col-lg-4 col-md-7 col-sm-6">
-                <div className="container border border-success-subtle border-4 rounded-5 col-12 row-5 position-relative align-items-stretch shadow-lg w-100 ps-lg-4  pe-lg-5 ps-sm-4  pe-sm-4 text-wrap overflow-hidden">
-                    <div className="row overflow-hidden ">
-                        <div className=" col-lg-5 col-sm-6 col-md-6 bg-primary rounded overflow-hidden">
-                        Most Popular
-                        </div>
-
-                        <div className="col-8 ">
-                            
-                        </div>
-                    </div>
-
-                    <div className="row font-weight-semibold text-success ">
-                        <div className="col-12">
-                            <h3 className="font-weight-bolder">Lorem Basic</h3>
-                            <h1>$400.40</h1>
-                            <p>Lorem Ipsum</p> <br></br>
-                            
-                            <p>Lorem Ipsum</p><br></br>
-                            <p> ---------------------------</p>
-                            <h5>LOREM IPSUM LOREM IPSUM</h5>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <h5>LOREM IPSUM LOREM</h5>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM </p>
-                            <h5>LOREM IPSUM LOREM</h5> <br></br>
-                            <br></br>
-                        </div>
-                    </div>
-                                <div className="d-flex justify-content-between  align-items-center   ">
-                                <button className="btn btn-primary align-items-center px-3" type="button">Register</button>
-                                <button className="btn btn-warning align-items-center px-3" type="button">Book Now</button>
-                                </div>
-
-                </div>
-        </div>
-
-        <div className="col-lg-4 col-md-7 col-sm-6">
-                <div className="container border border-success-subtle border-4 rounded-5 col-12 row-5 position-relative align-items-stretch shadow-lg w-100 ps-lg-4  pe-lg-5 ps-sm-4  pe-sm-4 text-wrap overflow-hidden">
-                    <div className="row overflow-hidden ">
-                        <div className=" col-lg-5 col-sm-6 col-md-6 bg-primary rounded overflow-hidden">
-                        Most Popular
-                        </div>
-
-                        <div className="col-8 ">
-                            
-                        </div>
-                    </div>
-
-                    <div className="row font-weight-semibold text-success ">
-                        <div className="col-12">
-                            <h3 className="font-weight-bolder">Lorem Basic</h3>
-                            <h1>$400.40</h1>
-                            <p>Lorem Ipsum</p> <br></br>
-                            
-                            <p>Lorem Ipsum</p><br></br>
-                            <p> ---------------------------</p>
-                            <h5>LOREM IPSUM LOREM IPSUM</h5>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <h5>LOREM IPSUM LOREM</h5>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM </p>
-                            <h5>LOREM IPSUM LOREM</h5> <br></br>
-                            <br></br>
-                        </div>
-                    </div>
-                                <div className="d-flex justify-content-between  align-items-center   ">
-                                <button className="btn btn-primary align-items-center px-3" type="button">Register</button>
-                                <button className="btn btn-warning align-items-center px-3" type="button">Book Now</button>
-                                </div>
-
-                </div>
-        </div>
-
-        <div className="col-lg-4 col-md-7 col-sm-6">
-                <div className="container border border-success-subtle border-4 rounded-5 col-12 row-5 position-relative align-items-stretch shadow-lg w-100 ps-lg-4  pe-lg-5 ps-sm-4  pe-sm-4 text-wrap overflow-hidden">
-                    <div className="row overflow-hidden ">
-                        <div className=" col-lg-5 col-sm-6 col-md-6 bg-primary rounded overflow-hidden">
-                        Most Popular
-                        </div>
-
-                        <div className="col-8 ">
-                            
-                        </div>
-                    </div>
-
-                    <div className="row font-weight-semibold text-success ">
-                        <div className="col-12">
-                            <h3 className="font-weight-bolder">Lorem Basic</h3>
-                            <h1>$400.40</h1>
-                            <p>Lorem Ipsum</p> <br></br>
-                            
-                            <p>Lorem Ipsum</p><br></br>
-                            <p> ---------------------------</p>
-                            <h5>LOREM IPSUM LOREM IPSUM</h5>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <h5>LOREM IPSUM LOREM</h5>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM IPSUM LOREM IPSUM</p>
-                            <p>LOREM </p>
-                            <h5>LOREM IPSUM LOREM</h5> <br></br>
-                            <br></br>
-                        </div>
-                    </div>
-                                <div className="d-flex justify-content-between  align-items-center   ">
-                                <button className="btn btn-primary align-items-center px-3" type="button">Register</button>
-                                <button className="btn btn-warning align-items-center px-3" type="button">Book Now</button>
-                                </div>
-
-                </div>
         
-        </div>
-   </div>
+         
 
+
+        
         </>
+       
         
 
         
@@ -166,4 +47,4 @@ const PromotionOffered = () => {
 
 
 };
-export default PromotionOffered;
+export default Serves;
