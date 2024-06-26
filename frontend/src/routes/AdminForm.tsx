efb5691d1b68dcec0175e5bcb9e584787531d509
bopbaby <ayoibk25@gmail.com>
2024-06-26 13:06:56 -0500

    Merged it

--- frontend/src/routes/AdminForm.tsx
+++ frontend/src/routes/AdminForm.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
 import { Form, Spinner } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+import { useAuth } from "../hooks/useAuth";
+import { Booking } from "../types";
 export default function AdminForm() {
+
+ const{ user } = useAuth(); 
 
 const [startTime, setStartTime] = useState<string>("09:00");
 const [endTime, setEndTime] = useState<string>("17:00");
 const [date, setDate] = useState<string>("2024-06-22")
 const [isLoading, setIsLoading] = useState<boolean>(false);
+const [bookings, setBookings] = useState<Booking[]>();
 
 function handleStartTimeChange(e: React.ChangeEvent<HTMLInputElement>){
   setStartTime(e.target.value);
@@ -49,22 +55,45 @@ async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
   }
   console.log(prepareBody);
 }
+
+  useEffect(() => {
+      const requestHeaders: HeadersInit = {
+        authorization: user?.authToken ?? "none"
+      }
+
+      fetch(import.meta.env.VITE_SERVER + '/bookings', {
+        headers: requestHeaders
+      })
+      .then(response => {
+        if (!response.ok) throw new Error('Bad server response')
+        console.log("Resposne " + response.statusText)
+        return response.json();
+      })
+      .then(responseJson => {
+        console.log(responseJson);
+        setBookings(responseJson)
+      })
+      .catch(error => {
+        console.error("Error fetching data", error);
+      })
+  }, []);
   
 
 return( 
-    <Form className=" bg-success p-4" onSubmit={handleSubmit}>
-      <h1 className=""> Admin Availability Schedule</h1>
+  <>
+      <Form className=" bg-success p-4" onSubmit={handleSubmit}>
+        <h1 className=""> Admin Availability Schedule</h1>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Date</Form.Label>
-        <Form.Control placeholder="Date" type="date" value={date} onChange={handleDateChange}/>
-      </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Date</Form.Label>
+          <Form.Control placeholder="Date" type="date" value={date} onChange={handleDateChange}/>
+        </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Start Time</Form.Label>
-        <Form.Control placeholder="Start Period /hr" type="time" value={startTime} onChange={handleStartTimeChange}/>
-        
-      </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Start Time</Form.Label>
+          <Form.Control placeholder="Start Period /hr" type="time" value={startTime} onChange={handleStartTimeChange}/>
+          
+        </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>End Time</Form.Label>
@@ -78,9 +107,38 @@ return(
       )}
     </Form>
    
+    <div>
+
+      <p>{JSON.stringify(bookings)}</p>
+      <Table striped bordered hover variant=" success" >
+      {/* <h3>Table Name</h3> */}
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Start Time</th>
+          <th>End Time</th>
+        </tr>
+
+        <tbody>
+        <tr>
+        <td>1</td>
+          {bookings?.map(bookTable => (
+          
+         
+        ))}
+        </tr>
+
+        </tbody>
+      </thead>
+ 
+   
+    </Table>
+   
+    </div>
+</>
+
+    );
 
 
-    )
-
-
-}
+} 
