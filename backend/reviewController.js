0b9f85eec574ae7a35e4ddc646c9fb382f4bb8a9
liam-kristjanson <liam.kristjanson@gmail.com>
2024-07-02 15:35:41 -0500

    Only allow review with valid key

--- backend/reviewController.js
+++ backend/reviewController.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const dateHelpers = require('./dateHelpers.js')
 const dbRetriever = require('./dbretriever.js')
 
@@ -7,11 +8,11 @@ module.exports.submitReview = async(req, res) => {
 
     try {
         if (!req.body.date || !req.body.name || !req.body.rating || !req.body.comments) {
-            return res.status(400).json({error: "Form fields are required!"});
+          return res.status(400).json({error: "Form fields are required!"});
         }
 
-        let chosenDate
-            //validate date format
+      let chosenDate
+      //validate date format
       try {
         chosenDate = dateHelpers.getDateFromStub(req.body.date);
       } catch (e) {
@@ -19,41 +20,32 @@ module.exports.submitReview = async(req, res) => {
         return res.status(400).json({error: "Invalid date format for start time, end time, or date"})
       }
 
-      //Intended line of code for querying Object ID
-      //const ObjectId = req.query.ObjectIdId;
-
       // Fetch existing review by Object ID
-      
 
-
-      let newReviewMade = {
-        date: chosenDate,
-        name: req.body.name,
-        rating: req.body.rating,
-        comments: req.body.comments
-        
-      }
-      
-      const endGoal = await dbRetriever.insertOne('reviews', newReviewMade);
-
-      if (endGoal.acknowledged) {
-        res.status(200).json({message: "Inserted record"});
-      } else {
-        res.status(500).json({error: "Error inserting reviews in database"});
+      try {
+        const objectId = new ObjectId(req.body.id);
+      } catch (e) {
+        return res.status(400).json({error: "Invalid review key"});
       }
 
-      const existingReview = await fetchDocumentById('reviews', reviewId);
-
+      const existingReview = await dbRetriever.fetchDocumentById('reviews', req.body.id);
+      
       if (existingReview) {
         console.log('Found existing review. Allowing update...');
-        dbRetriever.updateOne ('reviews', {existingReview: newReviewMade}) //complete this.
+
+        await dbRetriever.updateOne('reviews', {_id: new ObjectId(req.body.id)}, {$set: 
+          {
+            submittedDate: new Date(req.body.date),
+            rating: req.body.rating,
+            isSubmitted: true,
+            comments: req.body.comments
+          }});
+
         return res.status(200).json({message: "Review left successfully"})
-      
-      }else {
+      } else {
         console.log("No review found with id " + req.body.id + " aborting...");
         return res.status(400).json({error: "No review found with the given id"});
       } 
-      
           
     } catch (error) {
       console.error('Error handling review:', error);
